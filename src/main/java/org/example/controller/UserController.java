d0d3bb255f61b4223ea6902d300ae7c37e9f2649
renacala <152206619+renacalabrese@users.noreply.github.com>
2024-06-27 00:15:15 -0300

    registrar

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,17 +1,23 @@
 package org.example.controller;
 
+
+import org.springframework.ui.Model;
+import org.example.model.Category;
+import org.example.model.User;
 import org.example.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @Controller
 @RequestMapping("/")
 public class UserController {
-    /*
+
     @Autowired
     private UserService userService;
-    */
+
     @GetMapping()
     public String home() {
         return "index";
@@ -26,6 +32,38 @@ public class UserController {
     public String register() {
         return "register";
     }
+
+    @PostMapping("/register")
+    public String register(@RequestParam String userName,
+                           @RequestParam String password,
+                           @RequestParam String name,
+                           @RequestParam String address,
+                           @RequestParam String dni,
+                           Model model) {
+        // Verifica si el usuario ya existe
+        if (userService.existsUserName(userName)) {
+            model.addAttribute("error", "El usuario ya existe");
+            return "register";
+        } else {
+            // Crea un nuevo usuario
+            User user = new User();
+            user.setUserName(userName);
+            user.setPassword(password);
+            user.setName(name);
+            user.setAddress(address);
+            user.setDni(dni);
+            user.setCategory(Category.LOW); // Asigna una categoría por defecto (ejemplo: LOW)
+            user.setDayConnection(LocalDate.now()); // Establece la fecha actual de conexión
+            user.setRole("USER"); // Establece el rol como "USER"
+
+            // Registra el usuario utilizando el servicio UserService
+            userService.userRegister(user);
+
+            // Redirecciona al usuario a la página de login después de registrarse exitosamente
+            return "redirect:/login";
+        }
+    }
+
 
 }
 
--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -4,19 +4,20 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
-
+import java.time.LocalDate;
 
 @Document(collection = "users")
-
 @Data
 public class User {
     @Id
-    private Integer id;
-    private String name;
-    private String surname;
-    private String address;
-    private Integer phone;
+    private String Id;
     private String userName;
     private String password;
-    private int dni;
+    private String name;
+    private String address;
+    private String dni;
+    private LocalDate dayConnection;
+    private long dailyConnectionTime;
+    private Category category;
+    private String role;
 }
